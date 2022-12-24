c3461330c7e0b14c417666d7733cfcbaea2864ca
Milan <kricka.milan@gmail.com>
2022-12-24 13:50:22 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+col1,col2=st.columns(2)
+
+with col1:
+
+    st.image('images/photo.png')
+
+with col2:
+
+    st.title('Milan Kricka')
+
+    content="""Electrical Engineer with a demonstrated history of working in the information technology and services 
+    industry. Certified IBM Data Analyst. Strong engineering professional with high analytical skills, devoted 
+    to grow in field of Business Intelligence and Analysis. Devoted, disciplined, resourceful, creative and
+     adaptable. Oriented toward using sustainable technology solutions. """
+    st.info(content)
